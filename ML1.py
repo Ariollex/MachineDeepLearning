7f2b05c3a96fc1854c34baf0bfc6ecb49de698e6
Ariollex <ariollex@gmail.com>
2022-10-16 09:48:22 +0300

    Rework all passengers count

--- ML1.py
+++ ML1.py
@@ -1,19 +1,21 @@
 import pandas
 data = pandas.read_csv(filepath_or_buffer='Dataset/titanic.csv')
 
+# Всего пассажиров на корабле
+AllPassengers = data.PassengerId.max()
+
 # №1: Сколько мужчин было на корабле?
 male = data.Sex.value_counts()['male']
 print(male)
 
 # №2: Какая доля пассажиров (в %) выжила?
-All = data.Survived.value_counts()[1] + data.Survived.value_counts()[0]
-Survived = data.Survived.value_counts()[1]
-SurvivedPercent = int((Survived / All) * 100)
+SurvivedPassengers = data.Survived.value_counts()[1]
+SurvivedPercent = int((SurvivedPassengers / AllPassengers) * 100)
 print(SurvivedPercent, '%', sep='')
 
 # №3: Какая доля пассажиров (в %) от общего количества путешествовала во 2-ом классе?
 class2 = data.Pclass.value_counts()[2]
-class2Percent = int((class2 / All) * 100)
+class2Percent = int((class2 / AllPassengers) * 100)
 print(class2Percent, '%', sep='')
 
 # №4: Посчитайте среднее и медиану возраста всех людей на корабле.
